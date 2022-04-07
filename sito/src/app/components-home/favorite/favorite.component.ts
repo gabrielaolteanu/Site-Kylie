d055e65e57f3f2eaef2e23a57682e4c15d79d5ae
gabrielaolteanu <gabrielaolteanu15@gmail.com>
2022-04-07 13:07:01 +0200

    Finish favorite

--- sito/src/app/components-home/favorite/favorite.component.ts
+++ sito/src/app/components-home/favorite/favorite.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ICard } from 'src/app/model/card';
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
+_card: ICard = {
+  url: "",
+  text: "COLLECTION DELUXE",
+  type: "button"
+}
 
+
+@Input() set card (value: Partial<ICard>){
+  this._card={...this._card, ...value}
+}
   constructor() { }
 
   ngOnInit(): void {
---
+++ sito/src/app/model/card.ts
@@ -0,0 +1,5 @@
+export interface ICard {
+    url: string, 
+    text: string,
+    type: string
+}
